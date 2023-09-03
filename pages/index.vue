634a8be27ee718a7f18f32e72f43ddb94a7ba1be
Ali Bagheri <alibagheri.1379.1389@gmail.com>
2023-09-03 22:19:44 +0330

    seo update

--- pages/index.vue
+++ pages/index.vue
@@ -11,4 +11,19 @@ import FirstSection from '../components/home/FirstSection.vue';
 import SecondSection from '../components/home/SecondSection.vue';
 import FooterComponent from '../components/home/FooterComponent.vue';
 
+useHead({
+	title: 'Har Insight',
+	meta: [
+		{ name: 'title', content: 'Har Insight' },
+		{ name: 'type', content: 'website' },
+		{ name: 'url', content: 'https://harinsight.net' },
+		{ name: 'site_name', content: 'Har Insight' },
+		{ name: 'description', content: 'HAR file Analyzer' },
+		{ name: 'keywords', content: 'Har Insight, Har Analyze, HAR Analyzer, HAR file, HAR file analyzer, HAR, Insight, Analyze, Visualize' },
+		{ name: 'author', content: 'Ali Bagheru' },
+		{ name: 'robots', content: 'index, follow' },
+		{ name: 'googlebot', content: 'index, follow' },
+		{ name: 'google', content: 'notranslate' },
+	],
+})
 </script>
